e918c643c1a44c01270531e3345f8bdf5c344bd8
Abhideep Kumar <itsabhideep@gmail.com>
2024-01-02 18:22:59 +0530

    changed env variable name

--- components/login.js
+++ components/login.js
@@ -16,7 +16,7 @@ const Login = () => {
               You are signed in with <span className="text-yellow-500">{session.user.email}</span>
             </p>
             <button
-              onClick={() => signOut("google", { callbackUrl: process.env.NEXTAUTH_URL })}
+              onClick={() => signOut("google", { callbackUrl: process.env.ADMIN_NEXTAUTH_URL })}
               className="px-4 py-2 bg-red-500 rounded-md text-white font-medium shadow hover:bg-red-600 focus:outline-none"
             >
               Sign out
@@ -27,7 +27,7 @@ const Login = () => {
             <h1 className="text-2xl font-semibold mb-4">Join the Community</h1>
             <p className="text-gray-600 mb-4">Sign in with your Google account to access all features.</p>
             <button
-              onClick={() => signIn("google", process.env.NEXTAUTH_URL)}
+              onClick={() => signIn("google", process.env.ADMIN_NEXTAUTH_URL)}
               className="px-4 py-2 bg-blue-500 rounded-md text-white font-medium shadow hover:bg-blue-600 focus:outline-none"
             >
               Login with Google
